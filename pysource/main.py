483bbba75865d9275ecb09a64fc121e2f4abaf3b
Jesse Stone <fatalfeel@hotmail.com>
2024-09-30 14:31:17 +0800

    auto change path to run

--- pysource/main.py
+++ pysource/main.py
@@ -5,12 +5,17 @@ https://fred.stlouisfed.org/series/JTSJOL
 https://fred.stlouisfed.org/series/UNEMPLOY
 '''
 
+import os
 import setup
 import formatFigure
 import figure4
 
 # Set parameters
 if __name__ == '__main__':
+    currentPath = os.getcwd()
+    if 'pysource' not in currentPath:
+        os.chdir(os.getcwd()+'/pysource')
+
     setup.run()
     formatFigure.run()
     figure4.run()
